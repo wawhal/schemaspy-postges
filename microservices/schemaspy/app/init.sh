ae7927fa13024edfc3c8bcc08750187a4e773ddb
wawhal <rishichandra.wawhal@gmail.com>
2017-11-16 20:48:12 +0530

    DP

--- microservices/schemaspy/app/init.sh
+++ microservices/schemaspy/app/init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-java -jar schemaSpy.jar -t pgsql -s public -db hasuradb -u admin -p understand-photoengraving-sloughy-dahl -host postgres.hasura -o /usr/share/nginx/html -dp postgresql-jdbc4.jar
+java -jar schemaSpy.jar -t pgsql -s public -db hasuradb -u admin -p $POSTGRES_PASSWORD -host postgres.hasura -o /usr/share/nginx/html -dp postgresql-jdbc4.jar
 nginx -g daemon off;
